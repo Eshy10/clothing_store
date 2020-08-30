5192125a62c4d46574805bfeeb0a91a1daab2a31
Eshy10 <adewalebose15@gmail.com>
2020-08-30 08:18:53 +0100

    split homepage into components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component.jsx'
+import Homepage from './pages/homepage/homepage.component.jsx'
 
 function App() {
   return (
-    <div>
+    <div className = 'App'>
     <Homepage />
     </div>
   );
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from '../../directory/directory.componnet.jsx'
+
+const Homepage = () => (
+    <div className = 'hompage'>
+    <Directory />
+    </div>
+)
+
+export default Homepage;
